8f74c4ac2ebd9900f3f1ede037e1ea7c2a78055f
unknown <r23083vedant@dpsrkp.net>
2024-06-06 00:01:26 +0530

    Add some code

--- task3_js/script.js
+++ task3_js/script.js
@@ -1,23 +1,19 @@
-function validate() {
-    var name = document.getElementById('name');;
-    var email = document.getElementById('email');
-    var grade = document.getElementById('grade');
-    var date = document.getElementById('dob');
-    var about = document.getElementById('yourself');
-    var achievement = document.getElementById('achieve');
+var a = document.getElementById("name")
+var b = document.getElementById("grade")
+var c = document.getElementById("email")
+var d = document.getElementById("dob")
+var e = document.getElementById("yourself")
+var f = document.getElementById("achieve")
 
-    if(name.value == "" || email.value == "" || grade.value == "" || date.value == "" || about.value == "" || achievement.value == "");
-        {
-            alert("Please fill out all the fields.")
-            return false;
-        }
-        
-}
 
-function isInputLetter (evt) {
-    var ch = String.fromCharCode(evt.which);
-
-    if (/^[A-Za-z_ ]*$/.test(ch)) {
-        alert("Name can only contain letters and spaces!")
+function info() 
+{
+    if (a == "" || b == "" || c == "" || d == "" || e == "" || f == "") {
+        alert("Kindly fill all the fields.")
+        return false
+    }
+    else {
+        return true
     }
 }
+
